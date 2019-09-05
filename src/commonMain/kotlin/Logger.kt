37938e671d81a41638da64f49c0ecb085bb1a536
Renette Ros <supergirl0894+git@gmail.com>
2019-09-05 15:38:54 +0200

    Add JsName annotation to logger

--- src/commonMain/kotlin/Logger.kt
+++ src/commonMain/kotlin/Logger.kt
@@ -1,4 +1,7 @@
+import kotlin.js.JsName
+
 interface Logger {
 
+    @JsName("info")
     fun info(message: String)
 }
--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,7 +1,7 @@
 //You can have standalone functions
 fun main(args: Array<String>) {
 
-    LoggerFactory.getLogger().info("Hello Java SA!")
+    LoggerFactory.getLogger().info("Hello Kotlin Everywhere!")
 
     //Common Module contains pure kotlin code
     //The following will not compile
